8f0e803dd1029010a6a036c145ca5f9b840d8f70
I.Smal <serbov@pochta.ru>
2016-10-29 17:36:56 +0300

    all is almost ready

--- app/js/script.js
+++ app/js/script.js
@@ -1,2 +1,14 @@
 /*script*/
+$(window).scroll(function() {
+  if ($(this).scrollTop() >= 50) {
+    $('.arrow-to-top').fadeIn(200);
+  } else {
+    $('.arrow-to-top').fadeOut(200);
+  }
+});
+$('.arrow-to-top').click(function() {
+  $('body,html').animate({
+    scrollTop : 0
+  }, 500);
+});
 /*/script*/
